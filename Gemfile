9101f997b9a245aaf7a5fcbd9253d1e25c7c07c3
Shaun Dern <smdern@gmail.com>
2012-02-08 00:23:53 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0.rc1'
+gem 'sqlite3'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.0'
+  gem 'coffee-rails', '~> 3.2.0'
+
+  gem 'uglifier', '>= 1.0.3'
+  gem 'twitter-bootstrap-rails'
+  gem 'haml-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookAid::Application.config.secret_token = 'f20e5b0dcafed353d19f0b7510abfa0ac54df88eaa73339bff2c7f49f4f8a59be8aa45cadd73053672805427449c823894bfc387ca649c42268b6546ea8121bb'
