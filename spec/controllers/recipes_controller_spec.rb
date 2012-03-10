2fc90317c266a35ba388fa9c3cb920cffb609ab9
Shaun Dern <smdern@gmail.com>
2012-03-09 23:48:45 -0800

    adding specs

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -4,7 +4,18 @@ describe RecipesController do
   describe "#index" do
     it "returns status code 200" do
       get :index
-      response.code.should == :ok
+      response.status.should == 200
+    end
+  end
+
+  describe "#show" do
+    before do
+        Recipe.stub(:find) { 'foo' }
+    end
+
+    it "returns status code 200" do
+      get :show, :id => 'foo'
+      response.status.should == 200
     end
   end
 end
